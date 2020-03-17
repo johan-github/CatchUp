bebe1b0e64b02c745d5c79112112def3957bc366
Matthias <nasheim@hotmail.com>
2020-03-17 21:12:21 +0100

    Added: ChannelMessage to Repo and Services
      ChannelMessage
      ChannelMessagesController
      ChannelMessageRepo
      ChannelMessageService

--- src/main/java/com/example/demo/controllers/ChannelMessagesController.java
+++ src/main/java/com/example/demo/controllers/ChannelMessagesController.java
@@ -1,7 +1,20 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entities.ChannelMessage;
+import com.example.demo.services.ChannelMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ChannelMessagesController {
+
+    @Autowired
+    ChannelMessageService channelMessageService;
+
+    @GetMapping("/rest/channelmessages/{channel_id}")
+    public List<ChannelMessage> getAllMessagesByChannelsId(@PathVariable int channels_id) {
+        return channelMessageService.findAllMessagesByChannelsId(channels_id); }
+
 }
