34f43a7e2f9119fcdc3ed49ef94d5ab62b947a70
Matthias <nasheim@hotmail.com>
2020-03-18 10:35:01 +0100

    Edited ChannelRepo
    Made some small changes to ChannelRepo

--- src/main/java/com/example/demo/repositories/ChannelRepo.java
+++ src/main/java/com/example/demo/repositories/ChannelRepo.java
@@ -1,7 +1,10 @@
 package com.example.demo.repositories;
 
+import com.example.demo.entities.Channel;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ChannelRepo {
+public interface ChannelRepo extends CrudRepository<Channel, Integer> {
+
 }
