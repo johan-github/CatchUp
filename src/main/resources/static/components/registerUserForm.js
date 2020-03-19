ab05b915d51a8775780e8cd287ebe6dd37ed4c58
Helena Barmer <helena.barmer@gmail.com>
2020-03-19 15:34:22 +0100

    added component: registerUserForm.js

---
+++ src/main/resources/static/components/registerUserForm.js
@@ -0,0 +1,16 @@
+
+
+export default {
+    template:`
+    <form>
+        <div>
+            <label>Enter user name</label>
+        </div>
+        <div>
+            <label>Enter passform</label>
+        </div>
+    
+    </form>
+    `
+
+}
