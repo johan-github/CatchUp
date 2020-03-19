58710717cae667fd9dc8d7f64cdc72cfda1672c2
Hassan <850815h@gmail.com>
2020-03-19 11:17:39 +0100

    Error check

--- src/main/java/com/example/demo/repositories/ChannelRepo.java
+++ src/main/java/com/example/demo/repositories/ChannelRepo.java
@@ -4,7 +4,18 @@ import com.example.demo.entities.Channel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ChannelRepo extends CrudRepository<Channel, Integer> {
+
+    //public List<Channel> findAllBySpeciesIgnoreCase(String specie);
+
+    public Channel findById( int id );
+
+    public List<Channel> findByAccountId(int account_id );
+
+
 
 }
