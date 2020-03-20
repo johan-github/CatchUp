7bb465aa678eff7ca8f0ee28d8495ecf6a9f6511
Hassan <850815h@gmail.com>
2020-03-20 23:33:56 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,46 @@
+import navbar from './components/navbar.js'
+
+
+export default{
+    components:{
+        navbar
+    },
+
+
+    template:`
+
+        <section id="appContainer">
+
+            <div id="appButton">
+                <h3>| | |</h3>
+            </div>
+
+            <div id="appNav">
+                <navbar/>
+            </div>
+            
+
+            <main id="appMain">
+                <router-view/>
+            </main>
+            
+        </section>
+    
+    `,
+
+
+    /*async created(){
+        let channelNames = await fetch('/rest/channelnames')
+        channelNames = await channelNames.json()
+        
+        this.$store.commit('setChannelNames', channelNames)
+    }*/
+    
+}
+
+
+/* OROGINAL SOM SKALL TILLBAKA */
+/*
 export default {
 
   template: `
