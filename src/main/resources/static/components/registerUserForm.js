7249023eb9d3ec1cccf1ec714894c6b900e1a041
Alberts Gudermanis <albertsgudermanis@gmail.com>
2020-03-20 13:35:45 +0100

    Updated loginuser to match to registeruser

--- src/main/resources/static/components/registerUserForm.js
+++ src/main/resources/static/components/registerUserForm.js
@@ -2,15 +2,66 @@
 
 export default {
     template:`
-    <form>
+    <form @submit.prevent="registerNewUserForm" class="registerUser" >
         <div>
             <label>Enter user name</label>
+            <input placeholder="Username" v-model="addUserName">
+
+            <label>Enter password</label>
+            <input placeholder="Password" v-model="addPassword">
         </div>
-        <div>
-            <label>Enter passform</label>
-        </div>
-    
+        <button>Register</button>
     </form>
-    `
+    `,
+
+data() {
+
+    return {
+        addUserName: '',
+        addPassword: ''
+    }
+  },
+
+  methods: {
+    registerNewUserForm() {
+        console.log("TEST " + this.addUserName, this.addPassword)
+
+        let newUser = {
+        addUserName: this.addUserName,
+        addPassword: this.addPassword
+        }
+        console.log("TEST 2 " + newUser)
+    }
+      
+  }
+  
+
+ 
 
 }
+
+// methods: {
+
+//     addNewProject() {
+
+//       console.log(this.title, this.description)
+
+
+//       let project = {
+
+//         title: this.title, 
+
+//         description: this.description
+
+//       }
+
+
+//       this.$store.commit('appendProject', project)
+
+//       this.$router.push('/')
+
+//     }
+
+//   }
+
+// }
---
+++ src/main/resources/static/views/registerUser.js
@@ -0,0 +1,13 @@
+import registerUserForm from '../components/registerUserForm.js'
+
+export default {
+    components: {
+        registerUserForm
+    },
+
+    template: `
+    <div>
+    <registerUserForm />
+    </div>
+    `
+}
