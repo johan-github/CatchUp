4c1bfb85107671f7b465eb6a460cf7f7667bd209
Alberts Gudermanis <albertsgudermanis@gmail.com>
2020-03-24 13:39:52 +0100

    fixed bugs

--- src/main/java/com/example/demo/services/ChannelMessageService.java
+++ src/main/java/com/example/demo/services/ChannelMessageService.java
@@ -2,9 +2,7 @@ package com.example.demo.services;
 
 
 import com.example.demo.entities.ChannelMessage;
-import com.example.demo.entities.ChannelName;
 import com.example.demo.repositories.ChannelMessageRepo;
-import com.example.demo.repositories.ChannelNameRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
