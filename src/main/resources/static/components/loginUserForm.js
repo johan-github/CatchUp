193ef903a2dfca52c38d9f0431b21b6d3b830d0e
Tobias Severfelt <tobias.severfelt@hotmail.com>
2020-03-27 11:32:12 +0100

    warning if password does not match

--- src/main/resources/static/components/loginUserForm.js
+++ src/main/resources/static/components/loginUserForm.js
@@ -5,14 +5,16 @@ export default {
     <form @submit.prevent="loginCurrentUserForm" class="loginuserform">
     <h2>Please login to continue</h2>
         <div class="logindiv">
-        <i class="fa fa-user"></i>
+        <i class="fa fa-envelope"></i>
         <label >Enter your email</label>
-        <input class="logininput" placeholder="email" v-model="enterUserEmail">
+        <input class="logininput" placeholder="email" v-model="enterUserEmail" required>
         </div>
         <div class="logindiv">
         <i class="fa fa-key"></i>
         <label>Enter your password</label>
-        <input  class="logininput" placeholder="password" v-model="enterPassword" type="password">
+        <input  class="logininput" placeholder="password" v-model="enterPassword" type="password" required>
+        <h4 id="passwordalert" >{{ passwordAlert  }}</h4>
+
         </div>
         <button class="loginbutton">Login</button>
     </form>
@@ -25,7 +27,8 @@ export default {
         
         return {
             enterUserEmail: '',
-            enterPassword: ''
+            enterPassword: '',
+            passwordAlert: ''
         }
     },
 
@@ -47,12 +50,15 @@ export default {
             currentUser = await currentUser.json()
             
 
-            if (currentUser.password == this.enterPassword){
+            if (currentUser.password === this.enterPassword){
                 console.log(currentUser)
+                
 
                 this.$store.commit('setCurrentUser', currentUser)
                 this.$router.push('/home')
 
+            } else {
+                this.passwordAlert = 'Email or password are incorrect, please try again'
             }
 
             console.log(currentUser.password);
