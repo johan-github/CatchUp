cb1209d0169c121d56435dcc76ff75f283da7349
Johan <johanomwikstrom@gmail.com>
2020-03-31 10:29:43 +0200

    userLogOut-component. Just a shell

---
+++ src/main/resources/static/components/userLogOut.js
@@ -0,0 +1,67 @@
+
+export default {
+    template:`
+        <section>
+                
+        </section>    
+    `,
+
+
+
+    data() {
+        
+    },
+
+
+
+    methods: {
+        async logOutCurrentUser() {
+            
+            // let accountsFromDB = await fetch('/rest/accounts')
+            //     .then( accounts => accounts.json())
+
+        }
+    },
+
+
+    computed:{
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
