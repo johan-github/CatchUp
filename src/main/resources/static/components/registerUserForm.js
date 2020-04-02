190e13c069c65f0a0c9af2ba57c9ebba470d2e12
Hassan <850815h@gmail.com>
2020-04-02 13:24:45 +0200

    Changes to displayChannel.js
    Vue - template:
    displayChannel : added container for the displayChannel box
    displayChannel : added div for search field 
    displayChannel : added scrollContainer for displaying channels
    displayChannel : added div for channel friends
    displayChannel : added div for channel name
    displayChannel : added div for channel last message
    displayChannel : added button to create channel (routes to channels)
    
    
    Vue - data():
    displayChannel : channelIds[] is removed
    displayChannel : messages[] is removed
    displayChannel : myChannels[] is removed and method getMyChannels() is used instead to act as an array.
    
    
    Vue - methods:
    displayChannel : changed variables using "user" to "account"
    displayChannel : added comments for describing what a method is doing accountLoggedIn
    displayChannel : getCurrentAccountInfo() is removed
    
    
    Vue - created:
    displayChannel : fetched and stored data directly from DB into data(). No longer computing anything from $store
    displayChannel : removed fetch('/rest/messages')
    displayMsgByChannel : added a fetch to get all the messages by the current channel-id
    
    
    Vue - computed:
    displayChannel : removed "channels(){ return this.$store.state.channels }"
    displayChannel : removed "accountChannels(){ return this.$store.state.accountChannels }"
    displayChannel : removed "accountLoggedIn(){ return this.$store.state.accountLoggedIn }"
    
    
    CSS:
    displayChannel : devided displayChannel into boxes and added CSS to them
    displayChannel : add container to hold the whole component

---
+++ src/main/resources/static/components/registerUserForm.js
@@ -0,0 +1,133 @@
+/********************************* /
+* Orginal by Helena & Alberts. 2020-03-19
+* Last Edited by ......
+* Notes: Is for when registering a user. ./view/registerUser.js
+/**********************************/
+export default {
+    template: /* html */ `
+    <section class="registerUser">
+    <form @submit.prevent="registerNewUserForm" class="registerForm">
+
+        <label class="label">Register new user</label>
+            
+        <div class="registerUserFormFields">
+            <div class="inputForm">
+                <input class="input-field" type="email" placeholder="Enter your email" v-model="addEmail" required >
+            </div>
+
+            <div class="inputForm">
+                <input class="input-field" type="text" placeholder="Enter your nickname" v-model="addNickname" required >
+            </div>
+
+            <div class="inputForm">
+                <input class="input-field" type="password" placeholder="Enter your password" v-model="addPassword" required>
+            </div>
+
+            <div class="inputForm">
+                <input class="input-field" type="password" placeholder="Confirm your password" v-model="confirmPassword" required>
+                <h4 id="passwordalert" >{{ passwordAlert  }}</h4>
+            </div>
+            </div>
+
+            <button class="registerButton">Register</button>
+            
+        </form>
+    </section>
+    `,
+
+data() {
+    return {
+        addEmail: '',
+        addNickname: '',
+        addPassword: '',
+        defaultAvatar: 'http://158.174.120.227/CatchUp/avatar01.png',
+        defaultStatus: 'offline',
+        confirmPassword: '',
+        passwordAlert: ''
+    }
+  },
+
+  
+  methods: {
+
+   //Adds a new user to backend
+    async registerNewUserForm() {
+        console.log("In register form now ")
+
+         if( !this.addEmail.trim() && !this.addPassword.trim() && !this.addNickname.trim() ){
+             return
+         } 
+
+         // If password does not match
+        if(!this.addPassword === !this.confirmPassword){
+            console.log("ERROR")
+            this.passwordAlert = ''
+            this.passwordAlert = "Password does not match! "
+            console.log(this.addPassword)
+            console.log(this.confirmPassword)
+        }
+
+        if(this.addPassword === this.confirmPassword){
+            console.log("PASSWORD OK")
+
+        // Save variables to be added to database as an object newUser
+        let newUser = {
+        email: this.addEmail,
+        usernick: this.addNickname,
+        password: this.addPassword,
+        avatar:  this.defaultAvatar,
+        status: this.defaultStatus
+        }
+        console.log(newUser)
+
+ 
+        // Post object to database
+        let result = await fetch('/rest/accounts', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+          })
+          result = await result.json()
+
+        // Empty input boxes 
+        this.addEmail=''
+        this.addNickname=''
+        this.addPassword=''
+        this.confirmPassword=''
+
+        // Re-direct to login page
+        this.$router.push('/loginUser')
+      
+  }
+}
+}
+
+}
+
+// methods: {
+
+//     addNewProject() {
+
+//       console.log(this.title, this.description)
+
+
+//       let project = {
+
+//         title: this.title, 
+
+//         description: this.description
+
+//       }
+
+
+//       this.$store.commit('appendProject', project)
+
+//       this.$router.push('/')
+
+//     }
+
+//   }
+
+// }
---
+++ src/main/resources/static/views/registerUser.js
@@ -0,0 +1,18 @@
+/********************************* /
+* Orginal by Tobbe & Alberts
+* Last Edited by ......
+* Notes: Is for regitering a new account. Note to owner. Rename to registerAccount.
+/**********************************/
+import registerUserForm from '../components/registerUserForm.js'
+
+export default {
+    components: {
+        registerUserForm
+    },
+
+    template: `
+    <div>
+    <registerUserForm />
+    </div>
+    `
+}
