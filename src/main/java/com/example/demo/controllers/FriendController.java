450a5cf0886422a59685cea5ce69cb83cb5200c9
Tobias Severfelt <tobias.severfelt@hotmail.com>
2020-04-07 15:21:12 +0200

    Added to backend, we can now add friends to the database

--- src/main/java/com/example/demo/controllers/FriendController.java
+++ src/main/java/com/example/demo/controllers/FriendController.java
@@ -1,9 +1,21 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Channel;
+import com.example.demo.entities.Friend;
+import com.example.demo.services.FriendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FriendController {
 
+    @Autowired
+    FriendService friendService;
 
+    @PostMapping("/rest/friends")
+    public Friend addFriendToList(@RequestBody Friend friend ){
+        return friendService.addNewFriend( friend );
+    }
 }
--- src/main/java/com/example/demo/services/FriendService.java
+++ src/main/java/com/example/demo/services/FriendService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Friend;
 import com.example.demo.repositories.AccountRepo;
 import com.example.demo.repositories.FriendRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,5 +11,10 @@ public class FriendService {
 
     @Autowired
     FriendRepo friendRepo;
+
+    public Friend addNewFriend(Friend friend) {
+        return friendRepo.save(friend);
+    }
+
 
 }
