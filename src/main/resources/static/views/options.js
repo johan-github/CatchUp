363fa2d5d92dfe4d25df424415f49e2de07f070d
Johan <johanomwikstrom@gmail.com>
2020-04-08 14:26:51 +0200

    Small clean-up in Option

--- src/main/resources/static/views/options.js
+++ src/main/resources/static/views/options.js
@@ -46,37 +46,3 @@ export default{
     `,
 
 }
-
-
-
-/** OLD , Safe use if above not working/ugly // Johan
- * template:`
-        <div>
-            <h3>O P T I O N S</h3>
-            <div id="homeContainer">
-                <div id="appButton">
-                    <h3>| | |</h3>
-                </div>
-                <div id="appNav">
-                    <navBar/>
-                </div>
-                <main id="appMain">
-                    <div id="optionsChangeNick">
-                        <input id="optionsChangeNickField" type="text" placeholder="Change your nickname">            
-                        <input id="optionsChangeNickButton" type="submit" value="Change nick">
-                </div>
-
-                <form id="optionsBox" action="/action_page.php">
-                    <input id="optionsBoxTextField" type="text" placeholder="Change your nickname">            
-                    <input id="optionsBoxAddButton" type="submit" value="Upload avatar">
-                </form>
-
-                <form id="optionsBox" action="/action_page.php">
-                    <input id="optionsBoxTextField" type="text" placeholder="Change your nickname">            
-                    <input id="optionsBoxAddButton" type="submit" value="Change nick">
-                </form>
-                </main>
-            </div> <!-- end-tag homeContainer -->
-        </div>
-    `,
- */
